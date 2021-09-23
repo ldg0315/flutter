03fc88d28a39e5127b7433b6bc2493d06e1d2d8a
ldg0315 <ldg315@naver.com>
2021-09-23 14:15:57 +0900

    Add files via upload

---
+++ prac_4(Toast message)/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toast Message',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Toast message'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              flutterToast();
+            },
+            child: Text('Toast'),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue   // Toast 버튼 컬러 변경
+          ),
+            // color: Colors.blue,
+        ),
+      ),
+    );
+  }
+}
+
+void flutterToast(){
+  Fluttertoast.showToast(msg: 'Flutter',
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.redAccent,
+      fontSize: 20.0,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT
+  );
+}
